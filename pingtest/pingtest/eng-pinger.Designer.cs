7759d6205a73722e941901d6e8dff79b786e735b
BGK <gorkemkizilok@gmail.com>
2017-08-03 12:39:09 +0300

    Add files via upload

---
+++ pingtest/pingtest/eng-pinger.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace pingtest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "South East Asia, Singapore",
+            "Europe",
+            "United States",
+            "Australia",
+            "Russia",
+            "South America",
+            "South Africa",
+            "Middle East"});
+            this.comboBox1.Location = new System.Drawing.Point(139, 5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Region:";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(139, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ping Now";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Red;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 86);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(325, 175);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(232, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 25);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Clear Text";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(139, 58);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 25);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Ping Continuously";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(264, 58);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(55, 25);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Stop";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.ErrorImage")));
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 58);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(325, 261);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(341, 300);
+            this.MinimumSize = new System.Drawing.Size(341, 300);
+            this.Name = "Form1";
+            this.Text = "PingTester";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ pingtest/pingtest/eng-pinger.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace pingtest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool[] pingable = new bool[2];
+        List<string> metin = new List<string>();
+
+        Timer t = new Timer();
+        
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string selected = this.comboBox1.GetItemText(this.comboBox1.SelectedItem);
+            if (selected == "")
+            {
+
+            }
+            else
+            {
+                pingbul(selected);
+            }
+        }
+
+        private void pingbul(string x)
+        {
+
+            Ping p = new Ping();
+            PingReply[] pr = new PingReply[2];
+
+            
+
+            for (int i = 0; i < 2; i++)
+            {
+                pingable[i] = false;
+            }
+            
+
+            switch (x)
+            {
+
+                case "South East Asia, Singapore":
+                    //South East Asia, Singapore                   
+                    pr[0] = p.Send("103.28.54.1");
+                    metin.Add("Singapore                     ");
+                    metin.Add("Singapore                     ");                    
+                    pr[1] = p.Send("103.10.124.1");                    
+                    pingable[0] = pr[0].Status == IPStatus.Success;
+                    pingable[1] = pr[1].Status == IPStatus.Success;                  
+                    break;
+
+                case "Europe":
+                    //Europe  
+                    metin.Add("(EU West) Luxembourg");
+                    metin.Add("(EU East) Vienna          ");
+                    pr[0] = p.Send("146.66.152.1");
+                    pr[1] = p.Send("146.66.155.1");
+                    pingable[0] = pr[0].Status == IPStatus.Success;
+                    pingable[1] = pr[1].Status == IPStatus.Success;
+                    break;
+
+                case "United States":
+                    //United States
+                    metin.Add("(US West) Washington ");
+                    metin.Add("(US East) Sterling         ");
+                    pr[0] = p.Send("192.69.96.1");
+                    pr[1] = p.Send("208.78.164.1");
+                    pingable[0] = pr[0].Status == IPStatus.Success;
+                    pingable[1] = pr[1].Status == IPStatus.Success;
+                    break;
+
+                case "Australia":
+                    //Australia 
+                    metin.Add("(AU) Sydney                 ");
+                    metin.Add("");
+                    pr[0] = p.Send("103.10.125.1");                  
+                    pingable[0] = pr[0].Status == IPStatus.Success;
+                    break;
+
+                case "Russia":
+                    //Russia  
+                    metin.Add("(SW) Stockholm           ");
+                    metin.Add("");
+                    pr[0] = p.Send("146.66.156.1");
+                    pingable[0] = pr[0].Status == IPStatus.Success;
+                    break;
+
+                case "South America":
+                    //South America  
+                    metin.Add("(BR)                              ");
+                    metin.Add("(BR)                              ");
+                    pr[0] = p.Send("209.197.29.1");                    
+                    pr[1] = p.Send("209.197.25.1");                    
+                    pingable[0] = pr[0].Status == IPStatus.Success;
+                    pingable[1] = pr[1].Status == IPStatus.Success;
+                    break;
+
+                case "South Africa":
+                    //South Africa  
+                    metin.Add("(SA) Cape Town           ");
+                    metin.Add("(SA) Cape Town           ");
+                    pr[0] = p.Send("197.80.200.1");                    
+                    pr[1] = p.Send("196.38.180.1");                   
+                    pingable[0] = pr[0].Status == IPStatus.Success;
+                    pingable[1] = pr[1].Status == IPStatus.Success;
+                    break;
+
+                case "Middle East":
+                    //Middle East
+                    metin.Add("(UAE) Dubai                 ");
+                    metin.Add("");
+                    pr[0] = p.Send("185.25.183.1");                    
+                    pingable[0] = pr[0].Status == IPStatus.Success;                  
+                    break;
+                    
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                if (pingable[i])
+                {
+                    richTextBox1.Text += string.Format("{0}, {1} --- >> {2}ms. {3}", metin.ElementAt(i), pr[i].Address.ToString(), pr[i].RoundtripTime.ToString(), Environment.NewLine);               
+                }
+                else
+                {
+                    richTextBox1.Text += "";
+                }
+            }
+
+            richTextBox1.Text += "\n";
+            for (int i = 1; i >= 0 ; i--)
+            {
+                metin.RemoveAt(i);
+            }
+            
+
+           
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+            richTextBox1.SelectionStart = richTextBox1.Text.Length;
+            richTextBox1.ScrollToCaret();
+            richTextBox1.SelectAll();
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            t = new Timer()
+            {
+                Interval = 1000
+            };
+
+            string selected = this.comboBox1.GetItemText(this.comboBox1.SelectedItem);
+            if (selected == "")
+            {
+
+            }
+            else
+            {                
+                t.Start();
+                t.Tick += delegate (object _s, EventArgs _e)
+                {
+                    pingbul(selected);
+                };
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            t.Stop();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Bölge Seçin:";
+            button1.Text = "Ping Bul";
+            button2.Text = "Temizle";
+            button3.Text = "Devamlı Ping Bul";
+            button4.Text = "Dur";
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Select Region:";
+            button1.Text = "Ping Now";
+            button2.Text = "Clear Text";
+            button3.Text = "Ping Continuously";
+            button4.Text = "Stop";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
